0e4017b0e4e6ec43c1c4675706eca720ae03d95f
Jagminder-CENG <71683198+Jagminder-CENG@users.noreply.github.com>
2020-11-10 23:21:43 -0500

    Finished SemiDetached Part
    The semi detached part was the same as the other two: check boxes, submit and toasts.

---
+++ app/src/main/java/jagminder/sembi/jagmindersembilab2/SembiActivitySemiDetached.java
@@ -0,0 +1,57 @@
+package jagminder.sembi.N01300801;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class SembiActivitySemiDetached extends AppCompatActivity {
+    private CheckBox ch1,ch2,ch3,ch4;
+    private ImageButton i_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sembi_semidetach);
+
+        ch1 = findViewById(R.id.checkBox);
+        ch2 = findViewById(R.id.checkBox2);
+        ch3 = findViewById(R.id.checkBox3);
+        ch4 = findViewById(R.id.checkBox4);
+
+        i_button = findViewById(R.id.imagebutton);
+
+        i_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(ch1.isChecked()){
+
+                    Toast.makeText(SembiActivitySemiDetached.this,"Selected House 1",Toast.LENGTH_SHORT).show();
+                }
+
+                if(ch2.isChecked()){
+
+                    Toast.makeText(SembiActivitySemiDetached.this,"Selected House 2",Toast.LENGTH_SHORT).show();
+                }
+                if(ch3.isChecked()){
+
+                    Toast.makeText(SembiActivitySemiDetached.this,"Selected House 3",Toast.LENGTH_SHORT).show();
+                }
+                if(ch4.isChecked()){
+
+                    Toast.makeText(SembiActivitySemiDetached.this,"Selected House 4",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+
+
+
+
+    }
+}
